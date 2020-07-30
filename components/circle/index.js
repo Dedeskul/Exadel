3ee465057d09225110c412bec5f0ce10c92df3de
Dedeskul Vladymyr <vladymyr.dedeskul1995@gmail.com>
2020-07-30 11:44:51 +0300

    used classes syntax and ES6 rest and spread operators, created new function which creates new circle if cycle

--- components/circle/index.js
+++ components/circle/index.js
@@ -2,22 +2,8 @@ const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'coral', 'orange'];
 const tray = document.getElementById('tray');
 const MAX_COUNT = 7;
 
-function getRandomNumber(maxValue) {
-    return Math.floor(Math.random() * colors.length)
-}
-
-function getRandomColor() {
-    return colors[getRandomNumber(colors)]
-}
-
-function Circle(_tray) {
-    this.tray = _tray;
-    this.borderColor = null;
-    this.textColor = null;
-    this.backgroundColor = null;
-    this.element = null;
-}
-
+const getRandomNumber = (maxValue) => Math.floor(Math.random() * colors.length);
+const getRandomColor = () => colors[getRandomNumber(colors)];
 
 class Circle {
     constructor(_tray) {
@@ -31,7 +17,7 @@ class Circle {
 
 Circle.prototype.addNewElementToDom = function (clickedElement) {
     this.element = this.tray.appendChild(document.createElement('div'));
-    this.borderColor = this.getUniqueColor('','');
+    this.borderColor = this.getUniqueColor();
     this.tray.lastChild.className = `circle ${this.borderColor}`;
     this.element.addEventListener('dblclick', this.changeCircleBackground.bind(clickedElement));
 }
@@ -39,22 +25,18 @@ Circle.prototype.addNewElementToDom = function (clickedElement) {
 Circle.prototype.setTextToTrayChildElement = function (text) {
     this.element = this.tray.lastChild.appendChild(document.createElement('p'));
     this.tray.lastChild.lastChild.innerHTML = text;
-    this.textColor = this.getUniqueColor(this.borderColor, '');
+    this.textColor = this.getUniqueColor(this.borderColor);
     this.backgroundColor = this.getUniqueColor(this.borderColor, this.textColor);
     this.element.style.color = this.textColor;
 }
 
-Circle.prototype.getUniqueColor = function(borderColor, textColor){
+Circle.prototype.getUniqueColor = function (...args) {
     const randomColor = getRandomColor();
-    if(arguments[0] === ''){
-        return randomColor;
-    }else{
-        const colors = [...arguments];
-        if(colors.indexOf(randomColor) === -1){
-            return randomColor;
-        }else{
-            return this.getUniqueColor(borderColor, textColor);
-        }
+    const colors = [...args];
+    if (colors.indexOf(randomColor) === -1) {
+        return randomColor
+    } else {
+        return this.getUniqueColor(...args);
     }
 }
 
@@ -64,12 +46,16 @@ Circle.prototype.changeCircleBackground = function(event){
     }
 }
 
-function setCirclesToTray () {
+const createTrayChildCircle = () => {
+    let trayChildCircle = new Circle(tray);
+    trayChildCircle.addNewElementToDom(trayChildCircle);
+    trayChildCircle.setTextToTrayChildElement(trayChildCircle.borderColor);
+}
+
+const setCirclesToTray = () => {
     tray.innerHTML = null
     for (let i = 0; i < MAX_COUNT; i++) {
-        let trayChildCircle = new Circle(tray);
-        trayChildCircle.addNewElementToDom(trayChildCircle);
-        trayChildCircle.setTextToTrayChildElement(trayChildCircle.borderColor);
+        createTrayChildCircle();
     }
 }
 
